c8eacc1ea65bebcfa6a356492063e67334e56a77
jroberts3136 <Jroberts3136@gmail.com>
2018-03-01 08:50:44 -0500

    No time for message now, will update later

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,19 +8,35 @@ public class Main {
 
     public static void main(String[] args) throws IOException{
         String fileName = "input.txt";
-        Team [] teams = fileRead(fileName);
+        Scanner fileSearch = new Scanner(new File(fileName));
+        //fileSearch.nextLine(); If we have a header line
+        Team [] division1 = fileRead(fileName, fileSearch);             //Implemented like this so scanner position remains and teams can be split into their respective divisions
+        Team [] division2 = fileRead(fileName, fileSearch);
+        Team [] division3 = fileRead(fileName, fileSearch);
+        Team [] division4 = fileRead(fileName, fileSearch);
     }
 
-    public static Team[] fileRead(String fileName) throws IOException{  //Reads input file and returns array of teams
-        Scanner fileSearch = new Scanner(new File(fileName));
+    public static Team[] fileRead(String fileName, Scanner search) throws IOException{  //Reads input file and returns array of teams
+
         int indx = -1;
         Team [] teams = new Team[64];                                   //Creates array of teams
 
-        //fileSearch.nextLine(); If we have a header line
-        while (fileSearch.hasNext()){
+        for (int i = 0; i<16; i++){                                     //Each division will only have 16 teams to begin with
             indx++;
-            teams[indx] = new Team(fileSearch.nextLine());
+            teams[indx] = new Team(search.nextLine());
         }
         return teams;
+    }
+
+    public static Team determineWin (Team highSeed, Team lowSeed){ //Return team with higher compare score, in case of tie, return better seed
+        if (highSeed.compareScore > lowSeed.compareScore){
+            return highSeed;
+        }
+        else if (lowSeed.compareScore > highSeed.compareScore){
+            return lowSeed;
+        }
+        else{
+            return highSeed;
+        }
     }
 }
--- src/com/company/Team.java
+++ src/com/company/Team.java
@@ -3,17 +3,38 @@ package com.company;
 /**
  * Created by jroberts3136 on 2/28/18.
  */
-public class Team {
+public class Team implements Comparable<Team>{
+    public String name;
+    public double ppg;
+    public double opg;
+    public double ppgopg;
+    public double foulShots;
+    public double tenure;
+    public double rebound;
+    public double seed;
     public double compareScore;
 
     public Team(String data){
         String[] teamData = data.split("\\t");
+
+        name = teamData[0];
+        ppg = Double.parseDouble(teamData[1]);
+        opg = Double.parseDouble(teamData[2]);
+        ppgopg = ppg/opg;
+        foulShots = Double.parseDouble(teamData[3]);
+        tenure = Double.parseDouble(teamData[4]);
+        rebound = Double.parseDouble(teamData[5]);
+        seed = Double.parseDouble(teamData[6]);
         compareScore = calculateCompare(teamData);
     }
 
     public double calculateCompare(String[] data){
-
-        double score;
+        double score = (ppgopg * 3) + (foulShots) + (tenure * 0.5) + (rebound * 1.5);   //Sample calculation, subject to change
         return score;
+    }
+
+    @Override
+    public int compareTo(Team o) {
+        return return Double.compare(seed, o.seed);
     }
 }
